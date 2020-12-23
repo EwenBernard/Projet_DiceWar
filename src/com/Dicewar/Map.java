bb427b526327c80e587f2f8667f7a3fe3d0bf957
EwenBernard <ewen.bernard@efrei.net>
2020-12-23 22:30:54 +0100

    Update Map.java

--- src/com/Dicewar/Map.java
+++ src/com/Dicewar/Map.java
@@ -1,16 +1,42 @@
 package com.dicewar;
 
 public class Map {
-    ArrayList<ArrayList<Territory>> Map;
+    ArrayList<ArrayList<Territory>> map;
+
     final static int HEIGHT = 150;
     final static int WIDTH = 200;
 
-    Map(boolean random)
+    private BufferedReader file;
+    private String line;
+
+    Map()
     {
-        if(random)
-        {
-            
-        }
+        this.map = mapRandomGenerator(HEIGHT, WIDTH);
+       
     }
+
+    Map(String fileLoc)
+    {
+        this.file = new BufferedReader(new FileReader(fileLoc));
+        this.map = this.file.mapFromCsv();
+    }
+
+
+    public mapReader(String fileLoc) throws IOException
+    {
+        this.file = new BufferedReader(new FileReader(fileLoc));
+    }
+
+    private boolean readLine() throws IOException
+    {
+        this.line = this.file.readLine();
+        return this.line != null;
+    }
+
+    private mapFromCsv(String fileName)
+    {
+
+    }
+
 }
  
